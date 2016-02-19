bf46f9bbbaab5ab357b5b3366b07be523b95731b
Mario Celi <mceli@stackbuilders.com>
2016-02-19 13:56:49 -0500

    add show template for orders

--- eta_notifications_flask/views.py
+++ eta_notifications_flask/views.py
@@ -15,6 +15,18 @@ def order_index():
 
 @app.route('/order/<order_id>')
 def order_show(order_id):
-    orders = Order.query.all()
+    order = Order.query.get(order_id)
 
-    return render_template('index.html', orders=orders)
+    return render_template('show.html', order=order)
+
+@app.route('/order/<order_id>/pickup')
+def order_pickup(order_id):
+    order = Order.query.get(order_id)
+
+    return render_template('show.html', order=order)
+
+@app.route('/order/<order_id>/deliver')
+def order_deliver(order_id):
+    order = Order.query.get(order_id)
+
+    return render_template('show.html', order=order)
